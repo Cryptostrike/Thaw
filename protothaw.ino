5b98560c7b959124bac6fa98393d188a66d493e6
Cryptostrike <adamw192@live.com>
2014-11-04 12:23:37 +0000

    Coloured LED for temperature range
    Added LEDs to indicate a given temperature range, the range is room
    temperature for testing.

--- protothaw.ino
+++ protothaw.ino
@@ -1,28 +1,45 @@
+
 #include "DHT.h"
-#define DHTPIN 2     // what pin we're connected to
+#define DHTPIN   A2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define ledRed D3
+#define ledYellow D4
+#define ledGreen D5
+#define ledBlue D6
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);  
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledBlue, OUTPUT);   
   dht.begin();
 }
 
 void loop() {
   delay(2000);
-  float h = dht.readHumidity();
+  //float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    digitalWrite(ledBlue, HIGH); 
     return;
   }
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: "); 
-  Serial.print(t);
-  Serial.println(" *C ");
+  
+  // Show different coloured LED depending on desired temperature range.  
+  if (t > 20){
+    digitalWrite(ledGreen, HIGH);
+  }
+  
+  else (18 < t < 20){
+    digitalWrite(ledYellow, HIGH);
+  }
+  
+  else ( 18 < t){ 
+    digitalWrite(ledRed, HIGH);
+  }
 }
+
