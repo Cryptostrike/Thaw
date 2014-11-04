3529029a65f39cba3cd77cef037b7789a529e309
Cryptostrike <adamw192@live.com>
2014-11-04 23:39:32 +0000

    Logic and syntax
    Added LED logic

--- protothaw.ino
+++ protothaw.ino
@@ -1,11 +1,10 @@
-
 #include "DHT.h"
-#define DHTPIN   A2     // what pin we're connected to
+#define DHTPIN   2     // what pin DHT 22 is on
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-#define ledRed D3
-#define ledYellow D4
-#define ledGreen D5
-#define ledBlue D6
+int ledRed = 3;
+int ledYellow = 4;
+int ledGreen = 7;
+int ledBlue = 6;
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -19,27 +18,28 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
+  delay(2000); // read every 2000 ms
   //float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
+
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledGreen, LOW);
+
+  // Check if any reads failed and show blue
+  if (isnan(t)) {
     digitalWrite(ledBlue, HIGH); 
     return;
-  }
+  };
   
   // Show different coloured LED depending on desired temperature range.  
-  if (t > 20){
-    digitalWrite(ledGreen, HIGH);
+  
+  if (t > 25){digitalWrite(ledYellow, HIGH);
+  } else if (20 <= t <= 25){digitalWrite(ledRed, HIGH);
+  } else if (t < 20){digitalWrite(ledGreen, HIGH);
   }
   
-  else (18 < t < 20){
-    digitalWrite(ledYellow, HIGH);
-  }
-  
-  else ( 18 < t){ 
-    digitalWrite(ledRed, HIGH);
-  }
+  Serial.println(t); // Print temp for debugging
 }
 
