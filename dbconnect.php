b1f3cb8b939bb0c2462b4c407187c552480bef3c
Cryptostrike <Cryptostrike@users.noreply.github.com>
2014-11-12 16:48:56 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,5 +4,5 @@ $MyPassword = "password";  // enter your password for mysql
 $MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
 
 $dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
-$selected = mysql_select_db("test",$dbh);
+$selected = mysql_select_db("thaw",$dbh);
 ?>
