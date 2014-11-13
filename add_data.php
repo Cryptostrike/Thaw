1f885ce85b64fa023f84855d936eedf15339de9a
Cryptostrike <Cryptostrike@users.noreply.github.com>
2014-11-13 13:54:16 +0000

    Sensor counter

--- add_data.php
+++ add_data.php
@@ -1,13 +1,20 @@
 <?php
     // Connect to MySQL
     include("dbconnect.php");
+    
+    $counter=1;
+    while ( isset( $_GET["serial".$counter] ) )
+    {
+        // Prepare the SQL statement
+        $SQL = "INSERT INTO thaw.temperature (sensor ,celsius) VALUES ('".$_GET["serial"]."', '".$_GET["temperature"]."')";     
 
-    // Prepare the SQL statement
-    $SQL = "INSERT INTO thaw.temperature (sensor ,celsius) VALUES ('".$_GET["serial"]."', '".$_GET["temperature"]."')";     
-
-    // Execute SQL statement
-    mysql_query($SQL);
-
+        // Execute SQL statement
+         mysql_query($SQL);
+        
+        // Increase counter
+        $counter++;
+    }
+    
     // Go to the review_data.php (optional)
     header("Location: review_data.php");
 ?>
